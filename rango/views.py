5fc63133b997157f1973c4a7c54509684f22b986
Melissa Ho <2442814M@student.gla.ac.uk>
2020-02-12 15:19:25 +0000

    Chapter 4.4. (Part 2)

--- rango/views.py
+++ rango/views.py
@@ -20,4 +20,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/about.html'>Index</a>")
+    # return HttpResponse("Rango says here is the about page. <a href='/rango/about.html'>Index</a>")
+    return render(request, 'rango/about.html')
