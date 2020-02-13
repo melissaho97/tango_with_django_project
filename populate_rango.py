fc7f7702af6af8b51fef8043761295070801cc71
Melissa Ho <2442814M@student.gla.ac.uk>
2020-02-13 18:28:04 +0000

    Chap 5.5 - temp

--- populate_rango.py
+++ populate_rango.py
@@ -38,9 +38,9 @@ def populate():
     ]
 
     cats = {
-        'Python': {'pages': python_pages},
-        'Django': {'pages': django_pages},
-        'Other Frameworks': {'pages': other_pages}
+        'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+        'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+        'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
     }
 
     # If you want to add more categories or pages,
--- rango/models.py
+++ rango/models.py
@@ -6,7 +6,9 @@ from django.db import models
 class Category(models.Model):
     # Primary Key
     name = models.CharField(max_length=128, unique=True)
-
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    
     # Change Categorys into Categories
     class Meta:
         verbose_name_plural = 'Categories'
