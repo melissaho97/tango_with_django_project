1c053f78690306f6e975bd9cefdfc68a6fe294e0
Melissa Ho <2442814M@student.gla.ac.uk>
2020-02-13 17:00:13 +0000

    chapter 5.1 - 5.4

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+# Inherit from Model base class
+
+class Category(models.Model):
+    # Primary Key
+    name = models.CharField(max_length=128, unique=True)
+
+    # without the folowing, it will show as <Category: Category object> if you were to print() the object
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    # Foreign Key
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
