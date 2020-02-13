205d8760af5575aa64e41b8f8314e488e40e5ffe
Melissa Ho <2442814M@student.gla.ac.uk>
2020-02-13 20:33:04 +0000

    Chapter 5.5 - end

--- populate_rango.py
+++ populate_rango.py
@@ -49,7 +49,7 @@ def populate():
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category
     for cat, cat_data in cats.items(): # calling add_cat() and add_page() functions repeatedly
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -65,8 +65,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
