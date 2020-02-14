00753afe4591fd5d1aefc6e5be565e33668ce5b1
Melissa Ho <2442814M@student.gla.ac.uk>
2020-02-14 01:45:58 +0000

    chapter 6.3 (part 1)

--- rango/models.py
+++ rango/models.py
@@ -1,17 +1,22 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 # Inherit from Model base class
-
 class Category(models.Model):
     # Primary Key
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
     # Change Categorys into Categories
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
 
     # without the folowing, it will show as <Category: Category object> if you were to print() the object
     def __str__(self):
--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 # Input:    takes in a HttpRequest object from the django.http module
 # Output:   return a HttpResponse object that takes a string parameter representing the content of the page we wish to send to the client requesting the view
@@ -24,3 +25,30 @@ def about(request):
     # return HttpResponse("Rango says here is the about page. <a href='/rango/about.html'>Index</a>")
     # ### Chapter 4
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    # Create a context dictionary which we can pass
+    # to the template rendering engine.
+    context_dict = {}
+    try:
+        # Can we find a category name slug with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        # The .get() method returns one model instance or raises an exception.
+        category = Category.objects.get(slug=category_name_slug)
+        # Retrieve all of the associated pages.
+        # The filter() will return a list of page objects or an empty list.
+        pages = Page.objects.filter(category=category)
+        # Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+        # We also add the category object from
+        # the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category.
+        # Don't do anything -
+        # the template will display the "no category" message for us.
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    # Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context=context_dict)
