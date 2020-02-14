f5ec0a2956d34235dfcd7fc7f29144cdf619acaf
Melissa Ho <2442814M@student.gla.ac.uk>
2020-02-14 00:17:01 +0000

    chapter 6.1 to 6.2

--- rango/views.py
+++ rango/views.py
@@ -2,18 +2,22 @@
 #  import the HttpResponse object from the django.http module
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 # Input:    takes in a HttpRequest object from the django.http module
 # Output:   return a HttpResponse object that takes a string parameter representing the content of the page we wish to send to the client requesting the view
 def index(request):
-    # return HttpResponse("Rango says hey there partner!  <a href='/rango/about/'>About</a>")
-    # ### Chapter 4
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5] # in descending order, retrieve top 5 categories
+    print(category_list)
+    context_dict = {}
+    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
