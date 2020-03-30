f89af1340c1f1153d3ee62ad18bc5659b4db2a06
Melissa Ho <2442814M@student.gla.ac.uk>
2020-03-30 12:55:47 +0100

    update

--- rango/models.py
+++ rango/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
     name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, default=uuid.uuid1)
 
     def save(self, *args, **kwargs):
         if self.views < 0:
